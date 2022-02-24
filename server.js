d6acefe4bfb2bccdb0e7f0991aa5ee80b2fb3d07
Strawpple <Akrequillo@gmail.com>
2022-02-24 22:12:22 +0800

    update

--- server.js
+++ server.js
@@ -1,20 +1,56 @@
-const http = require('http');
-const fs = require('fs');
+// const http = require('http');
+// const fs = require('fs');
 
 var express = require('express');
 var app = express();
 
 
 app.set('view engine', 'ejs');
+
+
+
+
+
+
+//Weatherjs 
+
+var weather = require('weather-js');
+
+// Options:
+// search:     location name or zipcode
+// degreeType: F or C
+
+
+
+
+weather.find({search: 'Davao City, Ph', degreeType: 'C'}, function(err, result) {
+    if(err) console.log(err);
+      // res.render('views/index', country1);
+      console.log(JSON.stringify(result, null, 2));
+  });
+
+
+
+
+app.listen(3000, 'localhost', () => {
+    console.log("Server is running");
+});
+    
+
+
+
+
 // require('dotenv').config();
 
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname, '/public', 'index'));
+// })
 
-
-let countries = [
-    {id: 1, cname: 'Philippines', price: 2000},
-    {id: 2, cname: 'South Korea', price: 2000},
-    {id: 3, cname: 'Dell', price: 2000}
-]
+// let countries = [
+//     {id: 1, cname: 'Philippines', price: 2000},
+//     {id: 2, cname: 'South Korea', price: 2000},
+//     {id: 3, cname: 'Dell', price: 2000}
+// ]
 // let cities = [
 //     {id: 1, ciname: 'Carmen, Davao del Norte', price: 2000},
 //     {id: 2, ciname: 'Davao City, Davao del Sur', price: 2000},
@@ -39,7 +75,7 @@ let countries = [
 //             {fname: "Adrian", lname: "Requillo"}
 //         ],
 //     }
-//     res.render('index')
+//     res.render('index');
 // })
 
 
@@ -49,10 +85,10 @@ let countries = [
 
 //Node Server - Routing
 
-const server = http.createServer((req, res) =>{
-    console.log(req.url);
+// const server = http.createServer((req, res) =>{
+//     console.log(req.url);
 
-    res.setHeader('Content-Type', 'text/html');
+//     res.setHeader('Content-Type', 'text/html');
     // let urll = './views'
 
     // if (req.url == '/'){
@@ -73,40 +109,20 @@ const server = http.createServer((req, res) =>{
     //     urll += '404.html';
     //     res.statusCode = 404;
     
-    // }
+//     // }
 
-    fs.readFile('./index.html', (err, data) => {
-        if(err){
-            console.log(err);
-            res.end();
+//     fs.readFile('./index.html', (err, data) => {
+//         if(err){
+//             console.log(err);
+//             res.end();
 
-        }else{
-            res.end(data);
-        }
-    });
+//         }else{
+//             res.end(data);
+//         }
+//     });
 
-});
-
-
-
-
-
-//Weatherjs 
-
-// var weather = require('weather-js');
-// const res = require('express/lib/response');
-// const { stringify } = require('querystring');
-// Options:
-// search:     location name or zipcode
-// degreeType: F or C
- 
-// weather.find({search: 'Davao City, Ph', degreeType: 'C'}, function(err, result) {
-//   if(err) console.log(err);
-//     app.get('/', function (req, res) {
-//     let country = stringify.result
-//     res.render('views/index',{country});
-//     })
 // });
+
 
 //weatherjs get data
 
@@ -122,8 +138,19 @@ const server = http.createServer((req, res) =>{
 
 // })
 
+// console.log(__dirname);
+ 
+// app.get('/', function (req, res) {
+//     res.sendFile('./views/index.ejs', { root: __dirname });
+// });
 
-app.listen(3000, 'localhost', () => {
-    console.log("Server is running");
-});
-    
+// // app.get('/countries', function (req, res) {
+// //     res.sendFile('./views/countries.ejs', { root: __dirname });
+// // });
+// //
+// // app.get('/', function (req, res) {
+// //     res.sendFile('./views/other.ejs', { root: __dirname });
+// // });
+// app.use((req, res) =>{
+//     res.status(404).sendFile('./views/404.ejs', { root: __dirname });
+// });
