17d3db9d28b1601192ed6a60b46e5c06230367ac
Strawpple <Akrequillo@gmail.com>
2022-04-02 03:14:28 +0800

    Update
    -List (Dropdown) at 70%

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
 const express = require('express');
+const http = require('http');
+const fs = require('fs');
 
 var app = express();
 app.set('view engine', 'ejs');
 // app.use(express.static(__dirname + 'styles'));
+// console.log(__dirname);
 
+const server = http.createServer((req, res) => {
+    console.log(req.url);
+
+    res.setHeader('Content-Type', 'text/html');
+    let url1 = './views/';
+
+    if(req.url == '/'){
+        url1 += 'pages/index';
+        res.statusCode = 200;
+    } else if(req.url == '/adoption'){
+        url1 += 'pages/adoption';
+        res.statusCode = 200;
+    } else if(req.url == '/account'){
+        url1 += 'pages/account';
+        res.statusCode = 200;
+        res.end();
+    }
+
+    fs.readFile(url1, (err, data) => {
+        if(err){
+            console.log(err);
+            res.end();
+        }else{
+            res.end(data);
+        }
+    });
+
+
+});
 
 var admin = require("firebase-admin");
 
@@ -32,16 +64,25 @@ app.get('/adoption', async function (req, res){
     //     console.log(doc.id, '=>', doc.data());
     // })
 
+    // console.log(foradoption.docs);
+
+
+    
     let data ={
         url: req.url,
         itemData: foradoption.docs,
         
     }
-    res.render('adoption', data);
-
+    res.render('pages/adoption', data);
 
 });
 
+app.get('/account',function (req, res){
+    let data = {
+        url: req.url,
+    }
+    res.render('pages/account', data);
+});
 
 
 app.listen(3000, 'localhost', () => {
